63d1f20b7626eada4a60c63287885cdec86db2cc
ShazaElsayedIsmail11 <elsayedshaza9@gmail.com>
2026-05-10 17:37:54 +0300

    Add followup routes and controller
    - Added CRUD controller for followups
    - Added followup routes
    - Registered followup routes in app.js
    - Tested GET, POST, PUT, DELETE locally

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-
+const followupRoutes = require('./routes/followupRoutes');
 const app = express();
 
 const limiter = rateLimit({
@@ -15,6 +15,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(limiter);
+app.use('/api/followups', followupRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Med Agents API is running!' });
