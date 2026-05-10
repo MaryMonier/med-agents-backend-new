b7558e776803a910950e9521b5288768fe615c64
mary <marymonier94@gmail.com>
2026-05-11 00:33:07 +0300

    fix merge conflict with shaza branch

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const rateLimit = require('express-rate-limit');
 const errorHandler = require('./middleware/errorHandler');
 const piiSanitize = require('./middleware/piiSanitize');
 const authRoutes = require('./routes/auth.routes');
-const followupRoutes = require('./routes/followupRoutes');
 
 const app = express();
 
@@ -22,7 +21,6 @@ app.use(limiter);
 app.use(piiSanitize);
 
 app.use('/api/auth', authRoutes);
-app.use('/api/followups', followupRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Med Agents API is running!' });
