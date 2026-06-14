1a2c862ccefa175a9c26d2cf933668c0e509c09b
ThomasGeorguos <thomasgeorguos@gmail.com>
2026-06-15 00:53:57 +0300

    followps created automatically

--- src/routes/consultationRoutes.js
+++ src/routes/consultationRoutes.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
   createConsultation,
   getAllConsultations,
   getConsultationById,
   updateConsultation,
-  deleteConsultation
-} = require('../controllers/consultationController');
+  deleteConsultation,
+} = require("../controllers/consultationController");
 
-const authMiddleware = require('../middleware/auth.middleware');
+const authMiddleware = require("../middleware/auth.middleware");
 router.use(authMiddleware);
 
-router.route('/')
-  .get(getAllConsultations)    
-  .post(createConsultation);  
-router.route('/:id')
-  .get(getConsultationById)   
-  .put(updateConsultation)    
+router.route("/").get(getAllConsultations).post(createConsultation);
+router
+  .route("/:id")
+  .get(getConsultationById)
+  .put(updateConsultation)
   .delete(deleteConsultation);
 
 module.exports = router;
