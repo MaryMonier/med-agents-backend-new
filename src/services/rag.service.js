eefd546db499730f6ce7de1916c642212551843a
MaryMonier <marymonier94@gmail.com>
2026-07-05 14:13:11 +0300

    edit in rag and embedding

--- src/services/rag.service.js
+++ src/services/rag.service.js
@@ -133,19 +133,10 @@
 
 
 const { Pinecone } = require('@pinecone-database/pinecone');
-const OpenAI = require('openai');
-const { PINECONE_API_KEY, OPENAI_API_KEY } = require('../config/env');
+const { PINECONE_API_KEY } = require('../config/env');
+const { getEmbedding } = require('./embedding.service');
 
 const pinecone = new Pinecone({ apiKey: PINECONE_API_KEY });
-const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
-
-const getEmbedding = async (text) => {
-  const response = await openai.embeddings.create({
-    model: 'text-embedding-3-small',
-    input: text,
-  });
-  return response.data[0].embedding;
-};
 
 const retrieve = async (query, language = 'en', topK = 3) => {
   try {
